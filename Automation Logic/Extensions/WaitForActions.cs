3410217b53add8a3b3a9cfee20fac8b3b244827f
micdom95 <micdom95@gmail.com>
2022-05-11 19:14:33 +0200

    - Add Nuget Packages for Selenium and Waiters - Add Nuget Package for Fluent Assertions - Implementation of Extensions for Waiters

---
+++ Automation Logic/Extensions/WaitForActions.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwissHerbalTests.Common.Extensions
+{
+    public static class WaitForActions
+    {
+        public static void WaitForPageIsLoaded(this IWebDriver driver)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until((x) =>
+            {
+                return ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete");
+            });
+        }
+
+        public static IWebElement WaitUntilElementExists(this IWebDriver driver, By elementLocator, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementExists(elementLocator));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
+                throw;
+            }
+        }
+
+        public static IWebElement WaitUntilElementVisible(this IWebDriver driver, By elementLocator, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Element with locator: '" + elementLocator + "' was not found.");
+                throw;
+            }
+        }
+
+        public static IWebElement WaitUntilElementClickable(this IWebDriver driver, IWebElement webElement, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementToBeClickable(webElement));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Element with locator: '" + webElement.TagName + "' was not found in current context page.");
+                throw;
+            }
+        }
+
+        public static IAlert WaitUntilAlertIsDisplayed(this IWebDriver driver, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.AlertIsPresent());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Alert is not displayed.");
+                throw;
+            }
+        }
+    }
+}
