4b8b6a92599774e7114e4e6b9d2b11986849c72d
micdom95 <micdom95@gmail.com>
2022-05-14 16:33:52 +0200

    Add DriverSetup and DriverType, Concept of DropdownHandler

---
+++ Automation Logic/DropdownHandler.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Logic
+{
+    public class DropdownHandler
+    {
+        public IWebDriver driver { get; set; }
+        public SelectElement select { get; set; }
+
+        public DropdownHandler(IWebDriver driver, SelectElement select)
+        {
+            this.driver = driver;
+            this.select = select;
+        }
+
+        public void SelectElementByIndex(int index)
+        {
+            select.SelectByIndex(index);
+        }
+
+        public void SelectElementByValue(string value)
+        {
+            select.SelectByValue(value);
+        }
+
+        public void SelectElementByText(string text)
+        {
+            select.SelectByText(text);
+        }
+
+        public void DeselectAllElements()
+        {
+            select.DeselectAll();
+        }
+
+        public void DeselectElementByIndex(int index)
+        {
+            select.DeselectByIndex(index);
+            
+        }
+
+        public void DeselectElementByValue(string value)
+        {
+            select.DeselectByValue(value);
+        }
+
+        public void DeselectElementByText(string text)
+        {
+            select.DeselectByText(text);
+        }
+    }
+}
---
+++ Automation Logic/Setup/DriverSetup/DriverSetup.cs
@@ -0,0 +1,52 @@
+﻿using Automation_Logic.Setup.DriverSetup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationLogic.Setup
+{
+    public static class DriverSetup
+    {
+        public static IWebDriver ReturnDriver(DriverType driverType)
+        {
+            IWebDriver _driver;
+            switch (driverType)
+            {
+                case DriverType.Chrome:
+                    ChromeOptions chromeOptions = new ChromeOptions
+                    {
+                        AcceptInsecureCertificates = true,
+                        PageLoadStrategy = PageLoadStrategy.Normal
+                    };
+                    chromeOptions.AddArgument("--start-maximized");
+
+                    _driver = new ChromeDriver(chromeOptions);
+                    break;
+
+                case DriverType.FireFox:
+                    _driver = new FirefoxDriver();
+                    break;
+
+                case DriverType.Edge:
+                    EdgeOptions edgeOptions = new EdgeOptions()
+                    {
+                        AcceptInsecureCertificates = true,
+                        PageLoadStrategy = PageLoadStrategy.Normal,
+                    };
+
+                    _driver = new EdgeDriver(edgeOptions);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(driverType), driverType, null);
+            }
+            return _driver;
+        }
+    }
+}
