5a20cb5b2ab1a742737b34e3633b2c6c4a02afb4
micdom95 <micdom95@gmail.com>
2022-11-29 21:56:53 +0100

    Assertion for Translations

---
+++ TestAutomationFramework/Translations/Assertions/TranslationAssertions.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSuite.Enums;
+using TestSuite.Interfaces;
+using TestSuite.Model;
+
+namespace TestSuite.Translations.Assertions
+{
+    public static class TranslationAssertions
+    {
+        public static bool CheckIfTextCoitainsTranslation(this IWebElement webElement, string translationKey, ITranslationRepository translationRepository, Languages language = Languages.Polish)
+        {
+            TranslationModel translation;
+
+            try
+            {
+                translation = translationRepository.Translations.Where(x => x.TranslationKey == translationKey).First();
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Given Translation Key is not exists or is duplicated in Translation Repository");
+            }
+
+            switch (language)
+            {
+                case Languages.English:
+                    webElement.Text.Should().Contain(translation.EnglishText);
+                    return true;
+                case Languages.Polish:
+                    webElement.Text.Should().Contain(translation.PolishText);
+                    return true;
+            }
+
+            return true;
+        }
+    }
+}
